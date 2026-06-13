03085e627e40f3f7f748c3c49a17b87849e7ef8d
Aksahy Pant <pantaksahy111@gamil.com>
2026-06-13 09:01:20 +0530

    json added

--- csimport.py
+++ csimport.py
@@ -1,4 +1,9 @@
 import pandas as pd
 df= pd.read_csv("titanic.csv")  # read a CSV file into a DataFrame
 print(df)  # print the first and last 5 rows of the DataFrame
-#print(df.to_html())  # convert the DataFrame to an HTML table and print it
+#print(df.to_html())  # convert the DataFrame to an HTML table
+#  and print it
+json_df = pd.read_json("https://jsonplaceholder.typicode.com/todos/1")  # read JSON data from a URL into a DataFrame
+print(json_df)  # print the DataFrame created from JSON data
+
+#selection by columns
