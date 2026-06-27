496186c9beb33bc5632763a244f7e2225b2e01e9
Aksahy Pant <pantaksahy111@gamil.com>
2026-06-27 22:04:20 +0530

    aggregrate func

--- aggregrate.py
+++ aggregrate.py
@@ -1,2 +1,9 @@
 import pandas as pd
-df= pd.read_csv('t20.csv')
+df= pd.read_csv('pokemon.csv')
+print(df.mean(numeric_only=True))
+#all function
+#.sum() - returns the sum of the values for the requested axis
+#.min() - returns the minimum value for the requested axis
+#.max() - returns the maximum value for the requested axis
+#.std() - returns the standard deviation for the requested axis
+#.count() - returns the number of non-null values for the requested axis
